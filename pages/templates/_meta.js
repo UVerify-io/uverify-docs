de52bba1d7a2cc203206f9d46b8246bab12e1157
fabianbormann <fabian.bormann@cardanofoundation.org>
2026-07-04 20:07:36 +0200

    feat: add template tutorial, update policies and missing built-in templates

--- pages/templates/_meta.js
+++ pages/templates/_meta.js
@@ -1,4 +1,6 @@
 export default {
   'built-in': 'Built-in Templates',
   'building-a-template': 'Building a Custom Template',
+  'step-by-step-tutorial': 'Step-by-Step Tutorial',
+  'update-policies': 'Update Policies',
 };
